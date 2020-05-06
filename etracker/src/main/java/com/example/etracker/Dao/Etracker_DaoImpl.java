c31a06b97afba6ffc22486af58ae2c2b0556024f
Jainy Joy <52598380+JainyJoy@users.noreply.github.com>
2020-05-06 14:15:45 +0530

    Update Etracker_DaoImpl.java

--- etracker/src/main/java/com/example/etracker/Dao/Etracker_DaoImpl.java
+++ etracker/src/main/java/com/example/etracker/Dao/Etracker_DaoImpl.java
@@ -15,6 +15,30 @@ public class Etracker_DaoImpl implements Etracker_Dao {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	
+    private final String G1 = "SELECT SUM(CASE WHEN transaction_type=0 THEN amount END) AS TotalExpense, SUM(CASE WHEN transaction_type=1 THEN amount END) AS TotalIncome FROM t_transaction WHERE year(transaction_date) = year( current_date()) and user_id= ? ";
+    private final String G2 = "SELECT SUM(CASE WHEN transaction_type=0 THEN amount END) AS TotalExpense, SUM(CASE WHEN transaction_type=1 THEN amount END) AS TotalIncome FROM t_transaction WHERE year(transaction_date) = year( current_date()) and month(transaction_date) = month(current_date())and user_id=?";
+    private final String G3 = "SELECT monthname(transaction_date) as Month, SUM(CASE WHEN transaction_type=0 THEN amount END) AS TotalExpense, SUM(CASE WHEN transaction_type=1 THEN amount END) AS TotalIncome FROM t_transaction WHERE year(transaction_date) = year(current_date()) and user_id=? group by Month";
+    private final String G4 = "SELECT dayofmonth(transaction_date) as Day, SUM(CASE WHEN transaction_type=0 THEN amount END) AS TotalExpense, SUM(CASE WHEN transaction_type=1 THEN amount END) AS TotalIncome FROM t_transaction WHERE month(transaction_date) = month(current_date()) and year(transaction_date) = year(current_date()) and user_id=? group by Day";
+    
 	
+    public Map<String, java.lang.Object> graph1(int uSER_ID) {
+		
+		return jdbcTemplate.queryForMap(G1,uSER_ID);
+	}
+    
+	public Map<String, java.lang.Object> graph2(int uSER_ID) {
+		
+		return jdbcTemplate.queryForMap(G2,uSER_ID);
+	}
+	
+    public Collection<Map<String,java.lang.Object>> graph3(int uSER_ID) {
+		
+		return jdbcTemplate.queryForList(G3,uSER_ID);
+	}
+    
+    public Collection<Map<String,java.lang.Object>> graph4(int uSER_ID) {
+		
+		return jdbcTemplate.queryForList(G4,uSER_ID);
+	}
 
 }
