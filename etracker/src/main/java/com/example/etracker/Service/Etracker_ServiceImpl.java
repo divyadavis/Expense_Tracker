bfd8c7165f3cc637da2d4a8dcdb2cf4e4736076c
Jainy Joy <52598380+JainyJoy@users.noreply.github.com>
2020-05-06 14:09:03 +0530

    Update Etracker_ServiceImpl.java

--- etracker/src/main/java/com/example/etracker/Service/Etracker_ServiceImpl.java
+++ etracker/src/main/java/com/example/etracker/Service/Etracker_ServiceImpl.java
@@ -5,6 +5,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Etracker_ServiceImpl implements Etracker_Service{
+	
+	@Autowired
+	Etracker_Dao epTrackDao;
+	
+	
+	public Map<String, java.lang.Object> graph1(int uSER_ID){
+		return epTrackDao.graph1(uSER_ID);
+	}
+	public Map<String, java.lang.Object> graph2(int uSER_ID){
+		return epTrackDao.graph2(uSER_ID);
+	}
+	public Collection<Map<String,java.lang.Object>>  graph3(int uSER_ID){
+		return epTrackDao.graph3(uSER_ID);
+	}
+	
+	public Collection<Map<String,java.lang.Object>> graph4(int uSER_ID){
+		return epTrackDao.graph4(uSER_ID);
+	}
 
 	
 }
