96e8038a9efa603222b13ed66e7ed6f471c4a8b9
divyadavis <48674739+divyadavis@users.noreply.github.com>
2020-05-07 12:57:07 +0530

    Modified Dao to display transaction details

--- etracker/src/main/java/com/example/etracker/Dao/Etracker_Dao.java
+++ etracker/src/main/java/com/example/etracker/Dao/Etracker_Dao.java
@@ -6,6 +6,11 @@ public interface Etracker_Dao {
 	public Map<String, java.lang.Object> graph2(int uSER_ID);
 	public Collection<Map<String,java.lang.Object>>  graph3(int uSER_ID);
 	public Collection<Map<String,java.lang.Object>> graph4(int uSER_ID);
+	public Collection<Map<String,Object>> getIncome(int uSER_ID);
+
+	public Collection<Map<String, Object>> getExpense(int uSER_ID);
+
+	public Collection<Map<String, Object>> getIncomeExpense(int uSER_ID);
 
 
 }
