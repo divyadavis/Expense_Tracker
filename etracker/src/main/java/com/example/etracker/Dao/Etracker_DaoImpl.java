ed1a06b928e5987c7f4fabdd0071a3bff318071e
divyadavis <48674739+divyadavis@users.noreply.github.com>
2020-05-07 13:00:39 +0530

    Modified Etracker_DaoImpl to display transaction details

--- etracker/src/main/java/com/example/etracker/Dao/Etracker_DaoImpl.java
+++ etracker/src/main/java/com/example/etracker/Dao/Etracker_DaoImpl.java
@@ -22,11 +22,10 @@ public class Etracker_DaoImpl implements Etracker_Dao {
     
 	
     public Map<String, java.lang.Object> graph1(int uSER_ID) {
-		
+	    
 		return jdbcTemplate.queryForMap(G1,uSER_ID);
 	}
-    
-	public Map<String, java.lang.Object> graph2(int uSER_ID) {
+    public Map<String, java.lang.Object> graph2(int uSER_ID) {
 		
 		return jdbcTemplate.queryForMap(G2,uSER_ID);
 	}
@@ -40,5 +39,23 @@ public class Etracker_DaoImpl implements Etracker_Dao {
 		
 		return jdbcTemplate.queryForList(G4,uSER_ID);
 	}
+    private final String FETCH_INCOME = "SELECT T_TRANSACTION.ITEM AS ITEM,T_TRANSACTION.AMOUNT AS AMOUNT, T_TRANSACTION.TRANSACTION_DATE AS DATE, T_CATEGORY.CATEGORY_NAME AS CATEGORY FROM T_TRANSACTION JOIN T_CATEGORY ON T_TRANSACTION.CATEGORY_ID=T_CATEGORY.ID WHERE T_TRANSACTION.TRANSACTION_TYPE=0 AND T_TRANSACTION.USER_ID= ? ORDER BY T_TRANSACTION.TRANSACTION_DATE DESC";
+    private final String FETCH_EXPENSE = "SELECT T_TRANSACTION.ITEM AS ITEM,T_TRANSACTION.AMOUNT AS AMOUNT, T_TRANSACTION.TRANSACTION_DATE AS DATE, T_CATEGORY.CATEGORY_NAME AS CATEGORY FROM T_TRANSACTION JOIN T_CATEGORY ON T_TRANSACTION.CATEGORY_ID=T_CATEGORY.ID WHERE T_TRANSACTION.TRANSACTION_TYPE=1 AND T_TRANSACTION.USER_ID= ? ORDER BY T_TRANSACTION.TRANSACTION_DATE DESC";
+    private final String FETCH_INCOME_EXPENSE = "SELECT T_TRANSACTION.ITEM AS ITEM,T_TRANSACTION.AMOUNT AS AMOUNT, T_TRANSACTION.TRANSACTION_DATE AS DATE, T_CATEGORY.CATEGORY_NAME AS CATEGORY FROM T_TRANSACTION JOIN T_CATEGORY ON T_TRANSACTION.CATEGORY_ID=T_CATEGORY.ID WHERE T_TRANSACTION.USER_ID= ? ORDER BY T_TRANSACTION.TRANSACTION_DATE DESC";
+	
+
+    public Collection<Map<String, Object>> getIncome(int uSER_ID) {
+		return jdbcTemplate.queryForList(FETCH_INCOME, uSER_ID);
+	}
+    public Collection<Map<String, Object>> getExpense(int uSER_ID) {
+		return jdbcTemplate.queryForList(FETCH_EXPENSE, uSER_ID);
+	}
+
+     public Collection<Map<String, Object>> getIncomeExpense(int uSER_ID) {
+		return jdbcTemplate.queryForList(FETCH_INCOME_EXPENSE, uSER_ID);
+	}
+
+
+
 
 }
